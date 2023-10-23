71f6d04a9001f0b3e37b90b33e16e2fe3324eae1
Behruz <behruz@bictory.io>
2023-10-23 21:36:35 +0500

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,3 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { App } from "~/app/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export default App
